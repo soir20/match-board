46986bdd62b2a534b6d92d07bcdce1c71beadf70
soir20 <71418127+soir20@users.noreply.github.com>
2021-07-26 02:19:01 -0400

    Use map for board

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod piece;
 
+use std::collections::HashMap;
 use enumset::EnumSet;
+use crate::piece::{Piece, Pos};
 
 struct Game {
     score: u64
@@ -9,7 +11,7 @@ struct Game {
 struct Board {
     h_size: u32,
     v_size: u32,
-    pieces: vec![0; length]
+    pieces: HashMap<Pos, Piece>
 }
 
 #[cfg(test)]

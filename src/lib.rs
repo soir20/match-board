709cb6712dfb426e1af4d957b3c4f70a7e5cd3b3
soir20 <71418127+soir20@users.noreply.github.com>
2021-07-27 23:50:12 -0400

    Fix pattern match function return value

--- src/lib.rs
+++ src/lib.rs
@@ -15,22 +15,25 @@ pub struct MatchPattern {
 
 impl MatchPattern {
     pub fn find_match(&self, board: &Board, pos: Pos) -> Option<Vec<Pos>> {
-        self.spaces.keys().into_iter().find(|&&original| {
+        self.spaces.keys().into_iter().find_map(|&original|
             self.check_variant_match(board, pos - original)
-        });
-
-        Option::None
+        )
     }
 
-    fn check_variant_match(&self, board: &Board, new_origin: Pos) -> bool {
-        let original_to_board_pos = self.change_origin(new_origin);
-        original_to_board_pos.iter().all(|(original_pos, board_pos)|
+    fn check_variant_match(&self, board: &Board, new_origin: Pos) -> Option<Vec<Pos>> {
+        let mut original_to_board_pos = self.change_origin(new_origin);
+        let is_match = original_to_board_pos.iter().all(|(original_pos, board_pos)|
             match board.get_piece(*board_pos) {
                 None => false,
                 Some(piece) => piece.get_type() == self.spaces.get(original_pos)
                     .expect("Known piece wasn't found in pattern!")
             }
-        )
+        );
+
+        match is_match {
+            true => Some(original_to_board_pos.drain(..).map(|(_, board)| board).collect()),
+            false => None
+        }
     }
 
     fn change_origin(&self, origin: Pos) -> Vec<(Pos, Pos)> {

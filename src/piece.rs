9a127d861369cb190d01cee49f788f554400d256
soir20 <71418127+soir20@users.noreply.github.com>
2021-08-18 23:13:27 -0400

    Implement common traits for piece

--- src/piece.rs
+++ src/piece.rs
@@ -1,9 +1,10 @@
 use enumset::enum_set;
 use enumset::EnumSet;
 use enumset::EnumSetType;
+use std::fmt::{Display, Formatter};
 
 /// A unique category for board pieces.
-#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
 pub struct PieceType {
     name: &'static str
 }
@@ -18,10 +19,21 @@ impl PieceType {
         PieceType { name }
     }
 
+    /// Gets the name of this piece type.
+    pub fn name(&self) -> &str {
+        self.name
+    }
+
+}
+
+impl Display for PieceType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
 }
 
 /// A direction that a piece could move.
-#[derive(EnumSetType)]
+#[derive(EnumSetType, Ord, PartialOrd, Hash, Debug)]
 pub enum Direction {
     North = 0,
     South = 1,
@@ -43,6 +55,17 @@ impl Direction {
 
 }
 
+impl Display for Direction {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match *self {
+            Direction::North => "North",
+            Direction::South => "South",
+            Direction::East => "East",
+            Direction::West => "West"
+        })
+    }
+}
+
 pub const ALL_DIRECTIONS: EnumSet<Direction> = enum_set!(
     Direction::North | Direction::South | Direction::East | Direction::West
 );
@@ -50,7 +73,7 @@ pub const ALL_DIRECTIONS: EnumSet<Direction> = enum_set!(
 /// An individual, possibly-movable piece on a board that belongs to a category.
 ///
 /// Empty pieces are always movable, while walls are never movable.
-#[derive(Eq, PartialEq, Hash, Copy, Clone)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub enum Piece {
     Regular(PieceType, EnumSet<Direction>),
     Empty,
@@ -73,6 +96,25 @@ impl Piece {
 
     }
 
+}
+
+impl Display for Piece {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match *self {
+            Piece::Regular(piece_type, _) => first_char(piece_type.name()),
+            Piece::Empty => ' ',
+            Piece::Wall => '#'
+        })
+    }
+}
+
+/// Gets the first character of a string or returns a question mark if the string is empty.
+///
+/// # Arguments
+///
+/// * `str` - the string to get the first character of. May be empty
+fn first_char(str: &str) -> char {
+    str.chars().next().unwrap_or('?')
 }
 
 #[cfg(test)]

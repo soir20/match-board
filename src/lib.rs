1be1fe710d14909ba0bdb7bb159333c8ec2b0bf7
soir20 <71418127+soir20@users.noreply.github.com>
2021-09-04 22:24:58 -0400

    Remove unused game struct only containing a score

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,3 @@ pub mod position;
 pub mod matching;
 pub mod board_state;
 mod bitboard;
-
-struct Game {
-    score: u64
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}

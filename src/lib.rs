5d4d6024586da7f89fe260caf575f4f6753f5eaf
soir20 <71418127+soir20@users.noreply.github.com>
2023-07-27 01:19:05 -0400

    Fix ambiguous module name

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod matching;
 mod position;
 mod match_board;
 
-pub use position::*;
-pub use matching::*;
-pub use board::*;
-pub use match_board::*;
+pub use crate::position::*;
+pub use crate::matching::*;
+pub use crate::board::*;
+pub use crate::match_board::*;
